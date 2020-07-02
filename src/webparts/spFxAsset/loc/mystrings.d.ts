353cc4865f982e853dd37973974d08d3293418f1
OS-Lee <v-leeli@outlook.com>
2020-07-02 10:55:08 +0800

    Initial commit

---
+++ src/webparts/spFxAsset/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxAssetWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxAssetWebPartStrings' {
+  const strings: ISpFxAssetWebPartStrings;
+  export = strings;
+}
